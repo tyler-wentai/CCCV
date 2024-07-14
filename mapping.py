cc7bb3bc8bd7295f1be3e211a1503b10fa62d6df
tyler-wentai <teb6@rice.edu>
2024-07-13 23:36:19 -0700

    changed geopoints in mapping func

--- mapping.py
+++ mapping.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import geopandas as gpd
+from shapely.geometry import Point
 
-def mapper(include_bathymetry=True):
+def mapper(geopoints=None, include_bathymetry=True):
+    # arg geopoints: geo-referenced points to be drawn on top of map
+    # arg include_bathymetry: whether or not to draw ocean depth levels
     path_land = "/Users/tylerbagwell/Desktop/packages/Natural_Earth_quick_start/50m_cultural/ne_50m_admin_0_countries.shp"
     path_maritime_0 = "/Users/tylerbagwell/Desktop/packages/ne_10m_bathymetry_all/ne_10m_bathymetry_L_0.shp"
     path_maritime_200 = "/Users/tylerbagwell/Desktop/packages/ne_10m_bathymetry_all/ne_10m_bathymetry_K_200.shp"
@@ -15,7 +19,7 @@ def mapper(include_bathymetry=True):
     # construct plotting figure
     fig, ax = plt.subplots(figsize=(15, 10))
 
-    ax.grid(True, which='both', linestyle='--', linewidth=1.0, color='white', alpha=0.5, zorder=1)
+    ax.grid(True, which='both', linestyle='--', linewidth=0.5, color='white', alpha=0.5, zorder=1)
 
     gdf1.plot(ax=ax, edgecolor='gray', color='lightgray', linewidth=0.5, zorder=2)
     if (include_bathymetry==True):
@@ -23,8 +27,25 @@ def mapper(include_bathymetry=True):
         gdf3.plot(ax=ax, edgecolor=None, color='#a3d5ff', zorder=0)
         gdf4.plot(ax=ax, edgecolor=None, color='#83c9f4', zorder=0)
 
-    ax.set_xlim([-20.0, 170.0])
-    ax.set_ylim([-50.0, +50.0])
+    if (geopoints is not None):
+        geopoints.plot(ax=ax, color='red', marker='o', markersize=1, label='Points')
+
+    #ax.set_xlim([-20.0, 170.0])
+    #ax.set_ylim([-50.0, +50.0])
+
+    #ax.set_xlim([+90.0, 130.0])
+    #ax.set_ylim([-30.0, +50.0])
+    plt.tight_layout()
+    #plt.savefig('map.png', bbox_inches='tight', pad_inches=0.1)
     plt.show()
 
-mapper()
+
+
+data_path = "/Users/tylerbagwell/Documents/github/CCCV/data/pirate_attacks.csv"
+df_points = pd.read_csv(data_path)
+geometry = [Point(xy) for xy in zip(df_points['longitude'], df_points['latitude'])]
+gdf_points = gpd.GeoDataFrame(df_points, geometry=geometry)
+
+
+
+mapper(gdf_points)
