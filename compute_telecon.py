952fde53ad21fb3fc4cc83cad0fd9e87bce4e5ef
tyler-wentai <teb6@rice.edu>
2024-08-24 16:25:39 -0700

    .

--- compute_telecon.py
+++ compute_telecon.py
@@ -260,8 +260,133 @@ def compute_psi_Hsiang2011(climate_index, start_year, end_year, num_lag, num_R, 
     psi_array.to_netcdf(save_path)
 
 
-compute_psi_Hsiang2011(climate_index="amm",
-                       start_year=1980, end_year=2020, 
-                       num_lag=2, 
-                       num_R=3, 
-                       save_path="/Users/tylerbagwell/Desktop/psi_Hsiang2011_amm.nc")
+#
+def compute_gridded_correlation(climate_index, start_year, end_year, num_lag, save_path):
+    """
+    Computes Pearson correlation between climate_index and air temperature anomaly at 
+    all global grid points.
+    Output is a NetCDF file with description.
+    """
+
+    # Check if arguments have appropriate type:
+    if not isinstance(start_year, int):
+        raise TypeError(f"Expected an integer for start_year, but got {type(start_year).__name__}.")
+    if not isinstance(end_year, int):
+        raise TypeError(f"Expected an integer for end_year, but got {type(end_year).__name__}.")
+    if not isinstance(num_lag, int):
+        raise TypeError(f"Expected an integer for num_lag, but got {type(num_lag).__name__}.")
+
+    # Read in data files
+    file_path_AIR = '/Users/tylerbagwell/Desktop/air.2m.mon.mean.nc' # Air temperature anomaly
+    file_path_ONI = 'data/NOAA_ONI_data.txt' # ONI: Oceanic Nino Index
+    file_path_DMI = 'data/NOAA_DMI_data.txt' # DMI: Dipole Mode Index
+    file_path_AMM = 'data/NOAA_AMM_data.txt' # AMM: Atlantic Meridional Mode Index
+
+    start_date = datetime(start_year, 1, 1, 0, 0, 0)
+    end_date = datetime(end_year, 12, 1, 0, 0, 0)
+
+    # Read in climate index data file specified by climate_index
+    if climate_index=="oni" or climate_index=="ONI":
+        df_climate_index = prepare_ONI(file_path_ONI, start_date, end_date)
+        climate_index_name = 'oni'
+    elif climate_index=="dmi" or climate_index=="DMI":
+        df_climate_index = prepare_DMI(file_path_DMI, start_date, end_date)
+        climate_index_name = 'dmi'
+    elif climate_index=="amm" or climate_index=="AMM":
+        df_climate_index = prepare_AMM(file_path_AMM, start_date, end_date)
+        climate_index_name = 'amm'
+    else:
+        raise ValueError("Specified climate_index is not a valid climate index name.")
+    
+    # Read in and initialize the air temperature anomaly data
+    dat = nc.Dataset(file_path_AIR)
+
+    VAR1=dat.variables['air']
+    lat = dat.variables['lat'][:]
+    lon = dat.variables['lon'][:]
+    time = dat.variables['time'][:]
+
+    # Define the reference date: 1800-01-01 00:00:00. This is reference specified by NOAA's air.2m.mon.mean.nc file. 
+    reference_date = datetime(1800, 1, 1, 0, 0, 0)
+
+    dates = np.array([reference_date + timedelta(hours=int(h)) for h in time])
+    start_time_ind = int(np.where(dates == start_date)[0][0])
+    end_time_ind = int(int(np.where(dates == end_date)[0][0]) + 1)
+    VAR1 = VAR1[start_time_ind:end_time_ind, :, :]
+
+    VAR1_standard = np.empty_like(VAR1) # Initialize a new array to store the standardized data
+    n_time, n_lat, n_long = VAR1.shape
+    print("Original shape of air temp. data: ", n_time, n_lat, n_long)
+
+    # Loop through each (lat, long) point and standardize the time series at each grid point
+    print("Standardizing air temp. data...")
+    for i in range(n_lat):
+        if (i%100==0): 
+            print("...", i)
+        for j in range(n_long):
+            time_series = VAR1[:, i, j]
+            mean = np.mean(time_series)
+            std = np.std(time_series)
+            # Standardize the time series (avoid division by zero)
+            if std != 0:
+                VAR1_standard[:, i, j] = (time_series - mean) / std
+            else:
+                raise ValueError("std=0 at grid point: (", i, ",", j,")!")
+            
+    # Check if df_climate_index's and VAR1_standard's ts indicies are idential.
+    ind_time = df_climate_index.index.strftime('%Y-%m-%d').to_numpy()
+    vectorized_format = np.vectorize(lambda x: x.strftime('%Y-%m-%d'))
+    VAR1_time  = vectorized_format(dates[start_time_ind:end_time_ind])
+    if not np.array_equal(ind_time, VAR1_time):
+        raise ValueError("---The two date arrays of ind_time and VAR1_time (air temp.) are NOT identical.---")
+    else:
+        print("---The two date arrays of ind_time and VAR1_time (air temp.) are identical.---")
+
+    # Compute correlation(air temp, climate index) at each grid point
+    rho_all_months = np.empty((2, # index1: corr value; index2 = p-value
+                        VAR1_standard.shape[1],
+                        VAR1_standard.shape[2]))
+
+    for i in range(n_lat):
+        if (i%100==0): 
+            print("...", i)
+        for j in range(n_long):
+            df_help = pd.DataFrame({
+                'month': df_climate_index.index.month,
+                'ind_ts': df_climate_index['ANOM'],
+                'air_ts': VAR1_standard[:, i, j]})
+            lag_string = 'ind_ts_lag' + str(num_lag) + 'm'
+            df_help[lag_string] = df_help['ind_ts'].shift((num_lag))
+            df_help = df_help.dropna()
+            pearsonr_result = pearsonr(df_help[lag_string], df_help['air_ts'])
+            rho_all_months[0,i,j] = pearsonr_result[0]
+            rho_all_months[1,i,j] = pearsonr_result[1]
+
+    rho_array = xr.DataArray(data = rho_all_months,
+                            coords={
+                                "lat": lat,
+                                "lon": lon
+                            },
+                            dims = ["value", "lat", "lon"],
+                            attrs=dict(
+                                description="Rho, correlation between air temp and" + str(climate_index_name) + ". Indices: index1: corr value; index2 = p-value.",
+                                climate_index_used = climate_index_name,
+                                cor_calc_start_date = str(start_date),
+                                cor_calc_end_date = str(end_date),
+                                L_lag = num_lag)
+                            )
+
+    rho_array.to_netcdf(save_path)
+
+
+# compute_psi_Hsiang2011(climate_index="amm",
+#                        start_year=1980, end_year=2020, 
+#                        num_lag=2, 
+#                        num_R=3, 
+#                        save_path="/Users/tylerbagwell/Desktop/psi_Hsiang2011_amm.nc")
+
+
+compute_gridded_correlation(climate_index="amm", 
+                            start_year=1980, end_year=2020, 
+                            num_lag=0, 
+                            save_path="/Users/tylerbagwell/Desktop/rho_airVSamm_lag0.nc")
