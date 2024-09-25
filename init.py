18d9228aa9b1db915aba3e29ffba0833aecce0f6
tyler-wentai <teb6@rice.edu>
2024-09-24 22:33:44 -0500

    .

--- init.py
+++ init.py
@@ -239,10 +239,107 @@ def prepare_gridded_panel_data(regions, stepsize, num_lag, show_grid=False, show
 
     return final_gdf
 
-data = prepare_gridded_panel_data(regions='Africa', stepsize=1.0, num_lag=1, show_grid=True, show_gridded_tot_counts=False)
+# data = prepare_gridded_panel_data(regions='Africa', stepsize=1.0, num_lag=1, show_grid=True, show_gridded_tot_counts=False)
 
 # print(data.shape)
 # data.to_csv('/Users/tylerbagwell/Desktop/prepared_conflict_data.csv')
 
 # annual_index = compute_annualized_NINO3_index(1960, 2024)
 # print(annual_index)
+
+
+#######################################
+import h3
+import json
+
+regions = ['South Africa']
+stepsize = 1.0
+show_grid = True
+
+africa_countries = ['Mauritania','Western Sahara','Ivory Coast','Niger','Guinea-Bissau','Tunisia','Equatorial Guinea','Malawi','Gabon','Liberia',
+                        'Cabo Verde','Algeria','Lesotho','Sierra Leone','Mozambique','Ethiopia','Benin','Kenya','Guinea','Somalia','Madagascar','Comoros',
+                        'Morocco','Rwanda','South Sudan','Burkina Faso','Democratic Republic of the Congo','Botswana','Central African Republic','Nigeria',
+                        'Mali','Namibia','Libya','Senegal','Burundi','eSwatini','Cameroon','United Republic of Tanzania','Togo','Mauritius','Republic of the Congo',
+                        'Somaliland','Seychelles','Djibouti','Eritrea','Zimbabwe','Gambia','South Africa','Sudan','São Tomé and Principe','Zambia','Egypt',
+                        'Chad','Angola','Uganda','Ghana'] #Africa has 54 reconized countries + 2 territories (Somaliland and Western Sahara)
+
+# print(len(africa_countries))
+
+# read in shp file data
+path_land = "data/map_packages/50m_cultural/ne_50m_admin_0_countries.shp"
+gdf1 = gpd.read_file(path_land)
+
+# grab the polygons related to each country (SOVEREIGNT) and 'explode' any countries
+# made of multipolygons into individual polygons
+if (regions=='Africa' or regions=='africa'):
+    regions = africa_countries
+elif (regions=='Global' or regions=='global'):
+    regions = set(gdf1['SOVEREIGNT'])
+else:
+    if not isinstance(regions, list):
+        raise TypeError(f"'regions' argument should be a list if not a pre-specified region.")
+    regions = regions
+
+gdf1 = gdf1[gdf1['SOVEREIGNT'].isin(regions)]
+gdf1 = gdf1.explode(index_parts=True)
+exploded_polygons = [gdf1.iloc[i].geometry for i in range(gdf1.shape[0])]
+
+# Combine all individual polygons into a single multipolygon to ease the computation of the centroid,
+# centroid is used to form the grid around
+multi_polygon = shapely.geometry.MultiPolygon(exploded_polygons)
+gdf = gpd.GeoDataFrame(geometry=[multi_polygon])
+
+# compute region centroid and turn it into a gdf
+pnt = shapely.geometry.Point(multi_polygon.centroid.x, multi_polygon.centroid.y) 
+df = gpd.GeoDataFrame(geometry=[pnt], crs=4326)
+
+lon_center = df.geometry.iloc[0].x
+lat_center = df.geometry.iloc[0].y
+
+from hexalattice.hexalattice import *
+hex_centers, _ = create_hex_grid(n=10000, do_plot=True)
+centers_x = hex_centers[:, 0]
+centers_x = hex_centers[:, 1]
+plt.show()
+
+
+radius = 5000  # 5 km, Define the radius in meters
+
+
+sys.exit()
+
+# compute the min and max lon and lat values of the entire region, determins spatial extent of grid
+lon_min = np.min(gdf.geometry.get_coordinates()['x']); lon_max = np.max(gdf.geometry.get_coordinates()['x'])
+lat_min = np.min(gdf.geometry.get_coordinates()['y']); lat_max = np.max(gdf.geometry.get_coordinates()['y'])
+
+# compute the grids lat and lon start and end values by 'ceiling' the 
+# above computed region's min and max lon and lat values
+lon_start   = lon_center - np.ceil(lon_center-lon_min)
+lon_end     = lon_center + np.ceil(lon_max-lon_center) + stepsize
+lat_start   = lat_center - np.ceil(lat_center-lat_min)
+lat_end     = lat_center + np.ceil(lat_max-lat_center) + stepsize
+
+# compute coordinate mesh
+xcoords = np.arange(start=lon_start, stop=lon_end, step=stepsize)
+ycoords = np.arange(start=lat_start, stop=lat_end, step=stepsize)
+coords = np.array(np.meshgrid(xcoords, ycoords)).T.reshape(-1,2)
+
+# compute grid boxes' center points and create a GeoDataFrame of the grid
+centerpoints = gpd.points_from_xy(x=coords[:,0], y=coords[:,1])
+squares = [p.buffer(distance=(stepsize/2), cap_style=3) for p in centerpoints]
+df2 = gpd.GeoDataFrame(geometry=squares, crs=df.crs)
+
+# remove all grid boxes that do not contain a land regions
+df2 = df2[df2.intersects(gdf.geometry.iloc[0])]
+df2.reset_index(inplace=True)
+df2.drop('index', axis=1, inplace=True)
+df2['loc_id'] = ['loc_'+str(i) for i in range(df2.shape[0])]
+
+if (show_grid==True):
+    df = df.to_crs(4326)
+    df2 = df2.to_crs(4326)
+    ax = df.plot(color="violet", markersize=20, figsize=(6.5, 6.5), zorder=3)
+    df2.boundary.plot(ax=ax, zorder=2, color='black', linewidth=0.75)
+    gdf1.plot(ax=ax, color='lightgray', zorder=0, edgecolor='k', linewidth=0.75)
+    # plt.savefig('/Users/tylerbagwell/Desktop/grid_ex3.png', dpi=300, bbox_inches='tight', pad_inches=0.1)
+    plt.show()
