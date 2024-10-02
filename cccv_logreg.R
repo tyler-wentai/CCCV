9ea104a62f353199f0c2baf83ef2ba0466556f60
tyler-wentai <teb6@rice.edu>
2024-10-02 17:46:22 -0500

    .

--- cccv_logreg.R
+++ cccv_logreg.R
@@ -1,3 +1,6 @@
+library(brms)
+
+
 panel_data_path <- '/Users/tylerbagwell/Desktop/panel_data_AFRICA_binary.csv'
 dat <- read.csv(panel_data_path)
 
@@ -12,6 +15,7 @@ dat$year <- dat$year - min(dat$year)
 
 mod <- lm('conflict_binary ~ I(INDEX_lag0y*psi) + I((INDEX_lag0y*psi)^2) +
           I(INDEX_lag1y*psi) + I((INDEX_lag1y*psi)^2) +
+          I(INDEX_lag2y*psi) + I((INDEX_lag2y*psi)^2) +
           loc_id + loc_id*year', data=dat)
 summary(mod)
 
