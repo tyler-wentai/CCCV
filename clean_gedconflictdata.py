534b2657ef402795267e6b17454a3de006a1af2b
tyler-wentai <teb6@rice.edu>
2024-10-05 13:15:05 -0500

    .

---
+++ clean_gedconflictdata.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+print('\n\nSTART ---------------------\n')
+
+# Dataset: UCDP Georeferenced Event Dataset Codebook Version 24.1
+# URL: https://ucdp.uu.se/downloads/index.html#ged_global
+
+### Some column metadata:
+# sida_a (string):      The name of Side A in the dyad. In state-based conflicts always a government. In one-sided violence always the perpetrating party.
+# sida_b (string):      The name of Side B in the dyad. In state-based always the rebel movement or rivalling government. In one-sided violence always “civilians”.
+# where_prec (integer): The precision with which the coordinates and location assigned to the event reflects the location of the actual event.
+# best:                 The best (most likely) estimate of total fatalities resulting from an event.
+
+conflict_data_path = '/Users/tylerbagwell/Desktop/GEDEvent_v24_1.csv'
+df_conflict = pd.read_csv(conflict_data_path)
+
+print(df_conflict.shape)
+
+
+# step 1: remove conflicts between two or more governments, i.e., inter-state or state-on-state conflict
+df_cleaned = df_conflict[~(df_conflict['side_a'].str.startswith('Government') & df_conflict['side_b'].str.startswith('Government'))]
+
+# step 2: remove conflicts where there is large uncertainty in the event geo-location:
+df_cleaned = df_cleaned[df_cleaned['where_prec'] <= 4]
+
+# # step 3: remove extremely violent events with death counts above 15
+# df_cleaned = df_cleaned[df_cleaned['best'] <= 15]
+
+
+print(df_cleaned.shape)
+df_cleaned.to_csv('/Users/tylerbagwell/Desktop/GEDEvent_v24_1_CLEANED.csv', index=False)
+# df_cleaned.to_csv('/Users/tylerbagwell/Desktop/GEDEvent_v24_1_CLEANED_lowdeaths.csv', index=False)
