4088b08c9b2acf13f186ec454634fee6aaeb45b9
tyler-wentai <teb6@rice.edu>
2024-12-04 00:57:00 -0600

    .

--- init4.py
+++ init4.py
@@ -562,14 +562,14 @@ def prepare_gridded_panel_data(grid_polygon, localities, stepsize, nlag_psi, nla
 
 ### Hex stepsize = 0.620401 for an area of 1.0!!!
 
-panel_data = prepare_gridded_panel_data(grid_polygon='square', localities='Africa', stepsize=2,
+panel_data = prepare_gridded_panel_data(grid_polygon='hex', localities='Africa', stepsize=1.5,
                                         nlag_psi=5, nlag_conflict=1,
-                                        clim_index = 'NINO3',
+                                        clim_index = 'DMI',
                                         response_var='binary',
-                                        telecon_path = '/Users/tylerbagwell/Desktop/cccv_data/processed_teleconnections/psi_NINO3_cai_0d5.nc',
+                                        telecon_path = '/Users/tylerbagwell/Desktop/cccv_data/processed_teleconnections/psi_DMI_cai_0d5.nc',
                                         add_weather_controls=False,
                                         show_grid=True, show_gridded_aggregate=True)
-panel_data.to_csv('/Users/tylerbagwell/Desktop/panel_datasets/Binary_Africa_NINO3_square2_CON1_nocontrols.csv', index=False)
+panel_data.to_csv('/Users/tylerbagwell/Desktop/panel_datasets/Binary_Africa_DMI_hex1d5_CON1_nocontrols.csv', index=False)
 # panel_data.to_csv('/Users/tylerbagwell/Desktop/test_panel.csv', index=False)
 # print(panel_data)
 # nan_mask = panel_data.isna()
