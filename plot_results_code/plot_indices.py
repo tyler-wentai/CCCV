b1d6ca9724a4053b26acb58626675fc5b34b652b
tyler-wentai <teb6@rice.edu>
2025-04-09 18:43:35 -0500

    .

--- plot_results_code/plot_indices.py
+++ plot_results_code/plot_indices.py
@@ -37,20 +37,34 @@ nino3.rename(columns={'ANOM': 'NINO3'}, inplace=True)
 dmi.rename(columns={'ANOM': 'DMI'}, inplace=True)
 
 
+cmap = sns.diverging_palette(220, 20, as_cmap=True)
+num_colors = 5
+levels = np.linspace(0, 1, num_colors)
+colors = [cmap(level) for level in levels]
+
 
 # Create a figure with 2 subplots (one above the other), sharing the x-axis
 fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(4.5, 3.5), sharex=True)
 
 # Plot the NINO3 data on the first subplot
-ax1.plot(nino3.index, nino3['NINO3'], linestyle='-', linewidth=2, color='purple', label='NINO3', zorder=1)
-ax1.set_ylabel('NINO3')
+ax1.plot(nino3.index, nino3['NINO3'], linestyle='-', linewidth=1.5, color='purple', label='(Our ENSO index)', zorder=1)
+ax1.set_ylabel(r'NINO3 ($\degree C$)', fontsize=9)
+# ax1.legend(frameon=False, fontsize=8)
 
 ax1.axhline(0, color='black', linewidth=1.5, linestyle='--', zorder=2)
 
+ax1.axhspan(+0.5, +4.0, color=colors[4], alpha=0.20, edgecolor='none', linewidth=0.0, zorder=0)
+ax1.axhspan(-0.5, +0.5, color=colors[2], alpha=0.00, edgecolor='none', linewidth=0.0, zorder=0)
+ax1.axhspan(-4.0, -0.5, color=colors[0], alpha=0.20, edgecolor='none', linewidth=0.0, zorder=0)
+
+ax1.set_ylim(-2.5, 3.5)
+
+
 # Plot the DMI data on the second subplot
-ax2.plot(dmi.index, dmi['DMI'], linestyle='-', linewidth=2, color='green', label='DMI', zorder=1)
+ax2.plot(dmi.index, dmi['DMI'], linestyle='-', linewidth=1.5, color='green', label='(Our IOD index)', zorder=1)
 ax2.set_xlabel('Year', fontsize=9)
-ax2.set_ylabel('DMI', fontsize=9)
+ax2.set_ylabel(r'DMI ($\degree C$)', fontsize=9)
+# ax2.legend(frameon=False, fontsize=8)
 
 # Set the x-axis to show ticks every 10 years
 # Since we share the x-axis, set the locator on the bottom axes (ax2)
@@ -58,6 +72,9 @@ ax2.xaxis.set_major_locator(mdates.YearLocator(10))
 ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
 
 ax2.axhline(0, color='black', linewidth=1.5, linestyle='--', zorder=2)
+
+ax1.text(1990,  +3.0, 'El Niño', fontsize=9, color='k', horizontalalignment='center')
+ax1.text(1990,  -2.0, 'La Niña', fontsize=9, color='k', horizontalalignment='center')
 
 
 plt.tight_layout()
