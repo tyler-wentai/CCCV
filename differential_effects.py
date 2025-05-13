83c262e2e174d471c3cdfaf768052c8c9291ab40
tyler-wentai <teb6@rice.edu>
2025-05-12 23:34:43 -0500

    .

--- differential_effects.py
+++ differential_effects.py
@@ -7,96 +7,94 @@ import xarray as xr
 from shapely import wkt
 from shapely.geometry import Point
 import shapely
+from scipy.signal import detrend
 
 print('\n\nSTART ---------------------\n')
 
-# dat = pd.read_csv('/Users/tylerbagwell/Desktop/Psi_tp_directional_Onset_Count_Global_DMItype2_square4_wGeometry.csv')
-# dat_agg =dat.groupby('loc_id').agg({
-#     'psi': 'first',
-#     'psi_tp_directional': 'first',
-# }).reset_index()
+# # dat = pd.read_csv('/Users/tylerbagwell/Desktop/Psi_tp_directional_Onset_Count_Global_DMItype2_square4_wGeometry.csv')
+# # dat_agg =dat.groupby('loc_id').agg({
+# #     'psi': 'first',
+# #     'psi_tp_directional': 'first',
+# # }).reset_index()
 
-# psi_threshold = 0.25
-# mask = dat_agg['psi'] > psi_threshold
-# dat_agg = dat_agg.loc[mask]
+# # psi_threshold = 0.25
+# # mask = dat_agg['psi'] > psi_threshold
+# # dat_agg = dat_agg.loc[mask]
 
-# dat_agg['psi_tp_directional'] = np.abs(dat_agg['psi_tp_directional'])
+# # dat_agg['psi_tp_directional'] = np.abs(dat_agg['psi_tp_directional'])
 
+# # plt.figure()
+# # plt.scatter(dat_agg['psi_tp_directional'], dat_agg['psi'])
+# # plt.show()
+
+# # print(np.corrcoef(dat_agg['psi_tp_directional'], dat_agg['psi']))
+
+# # sys.exit()
+
+# dat = pd.read_csv('/Users/tylerbagwell/Desktop/differential_MEANanom_tp_NINO3.csv')
+# print(dat)
+
+# dat = dat[dat['psi'] > 0.5]
+
+# stddev = np.std(dat['cindex_lag0y'])
+# print(stddev)
+
+
+# mask1 = dat['psi_tp_directional'] > +0.5
+# anom1 = dat.loc[mask1]
+# mask1 = anom1['cindex_lag0y'] < -1.0*stddev
+# anom1 = anom1.loc[mask1, 'anom_tp']
+
+# # mask2 = dat['psi_tp_directional'] < 0
+# # anom2 = dat.loc[mask2]
+# # mask2 = anom2['cindex_lag0y'] < +1.5*stddev
+# # anom2 = anom2.loc[mask2, 'anom_tp']
+
+# mask2 = dat['psi_tp_directional'] < -0.5
+# anom2 = dat.loc[mask2]
+# mask2 = anom2['cindex_lag0y'] > +1.0*stddev
+# anom2 = anom2.loc[mask2, 'anom_tp']
+
+# mean1 = np.mean(anom1)
+# mean2 = np.mean(anom2)
+
+
+# # Plot overlapping histograms
 # plt.figure()
-# plt.scatter(dat_agg['psi_tp_directional'], dat_agg['psi'])
+# plt.hist(anom1, bins='scott', alpha=0.5, label='Pos. corr. w/ NINO3', color='darkorange', density=True)
+# plt.hist(anom2, bins='scott', alpha=0.5, label='Neg. corr. w/ NINO3', color='blue', density=True)
+# plt.axvline(mean1, color='darkorange', linestyle='--', linewidth=2, label=f'{mean1:.2f}')
+# plt.axvline(mean2, color='blue', linestyle=':', linewidth=2, label=f'{mean2:.2f}')
+# plt.xlim(-3,4.0)
+# plt.legend(title='Teleconnected regions whose\nprecipitation anomalies are:')
+# plt.xlabel('dryer  ←  Precipitation anomaly (s.d.)  →  wetter')
+# plt.ylabel('Density')
+# plt.title('NINO3 induced WETTING')
+
+# plt.tight_layout()
+# # plt.savefig('/Users/tylerbagwell/Desktop/NINO3_wetting.png', dpi=300, bbox_inches='tight', pad_inches=0.1)
 # plt.show()
 
-# print(np.corrcoef(dat_agg['psi_tp_directional'], dat_agg['psi']))
+# # conflict_dat = dat[dat['conflict_count']==1]
+# # print(conflict_dat)
+
+# # plt.figure()
+# # plt.hist(conflict_dat['anom_tp'], bins='scott', alpha=0.5, color='darkorange', density=False)
+# # plt.legend()
+# # plt.xlabel('anom_tp')
+# # plt.show()
+
 
 # sys.exit()
 
-dat = pd.read_csv('/Users/tylerbagwell/Desktop/differential_MEANanom_tp_NINO3.csv')
-print(dat)
-
-dat = dat[dat['psi'] > 0.5]
-
-stddev = np.std(dat['cindex_lag0y'])
-print(stddev)
-
-
-mask1 = dat['psi_tp_directional'] > +0.5
-anom1 = dat.loc[mask1]
-mask1 = anom1['cindex_lag0y'] < -1.0*stddev
-anom1 = anom1.loc[mask1, 'anom_tp']
-
-# mask2 = dat['psi_tp_directional'] < 0
-# anom2 = dat.loc[mask2]
-# mask2 = anom2['cindex_lag0y'] < +1.5*stddev
-# anom2 = anom2.loc[mask2, 'anom_tp']
-
-mask2 = dat['psi_tp_directional'] < -0.5
-anom2 = dat.loc[mask2]
-mask2 = anom2['cindex_lag0y'] > +1.0*stddev
-anom2 = anom2.loc[mask2, 'anom_tp']
-
-mean1 = np.mean(anom1)
-mean2 = np.mean(anom2)
-
-
-# Plot overlapping histograms
-plt.figure()
-plt.hist(anom1, bins='scott', alpha=0.5, label='Pos. corr. w/ NINO3', color='darkorange', density=True)
-plt.hist(anom2, bins='scott', alpha=0.5, label='Neg. corr. w/ NINO3', color='blue', density=True)
-plt.axvline(mean1, color='darkorange', linestyle='--', linewidth=2, label=f'{mean1:.2f}')
-plt.axvline(mean2, color='blue', linestyle=':', linewidth=2, label=f'{mean2:.2f}')
-plt.xlim(-3,4.0)
-plt.legend(title='Teleconnected regions whose\nprecipitation anomalies are:')
-plt.xlabel('dryer  ←  Precipitation anomaly (s.d.)  →  wetter')
-plt.ylabel('Density')
-plt.title('NINO3 induced WETTING')
-
-plt.tight_layout()
-# plt.savefig('/Users/tylerbagwell/Desktop/NINO3_wetting.png', dpi=300, bbox_inches='tight', pad_inches=0.1)
-plt.show()
-
-# conflict_dat = dat[dat['conflict_count']==1]
-# print(conflict_dat)
-
-# plt.figure()
-# plt.hist(conflict_dat['anom_tp'], bins='scott', alpha=0.5, color='darkorange', density=False)
-# plt.legend()
-# plt.xlabel('anom_tp')
-# plt.show()
-
-
-sys.exit()
-
-
+############ ------- ############
 ############ COMPUTE ############
+############ ------- ############
 
-path = '/Users/tylerbagwell/Desktop/cccv_data/processed_climate_data/tp_anom_ERA5_0d5_19502023_wTimeLatLon.nc'
 
-var1 = xr.open_dataset(path)
-# var1_yr = var1.resample(time="1Y").mean()
-# var1_yr = var1_yr.groupby("time.year").mean(dim="time")
-
-var1_yr = var1.sel(time=var1.time.dt.month.isin(range(5,13))) #grab months may-dec
-var1_yr = var1_yr.groupby("time.year").mean(dim="time")
+### --- 1: This var1_yr is dervied from compute_yr_anom.py
+path_var1_yr = '/Users/tylerbagwell/Desktop/cccv_data/processed_climate_data/ERA5_tp_YearlySumMayDec_0d50_19502023.nc'
+var1_yr = xr.open_dataset(path_var1_yr)
 
 
 ###
@@ -106,9 +104,6 @@ df['geometry'] = df['geometry'].apply(wkt.loads)
 
 gdf = gpd.GeoDataFrame(df, geometry='geometry')
 gdf.set_crs(epsg=4326, inplace=True)
-
-# psi_threshold = 0.471
-# gdf = gdf[gdf['psi'] >= psi_threshold]
 
 cols = ['loc_id', 'year', 'conflict_count', 'SOVEREIGNT', 'cindex_lag0y', 'psi', 'geometry']
 gdf = gdf[cols]
@@ -144,10 +139,19 @@ gdf.rename(columns={'psi_left': 'psi_tp_directional'}, inplace=True)
 # df.to_csv("/Users/tylerbagwell/Desktop/Psi_tp_directional_Onset_Count_Global_DMItype2_square4_wGeometry.csv", index=False)
 # print(gdf)
 
+gdf_agg = gdf.groupby('loc_id', as_index=False).first()
+gdf_agg = gdf_agg[['loc_id', 'geometry']]
+gdf_agg = gdf_agg.set_crs("EPSG:4326", allow_override=True)
+
 
 ###
-print(set(gdf['year']))
+min_year = np.min(list(set(gdf['year'])))
+max_year = np.max(list(set(gdf['year'])))
 
+print(min_year, max_year)
+
+
+###
 anom_by_year = {}
 for yr in set(gdf['year']):
     print("...", yr)
@@ -157,7 +161,7 @@ for yr in set(gdf['year']):
     anom = anom[varname]
 
     anom_df = anom.to_dataframe(name="value").reset_index()
-    anom_df["geometry"] = [Point(xy) for xy in zip(anom_df.lon, anom_df.lat)]
+    anom_df["geometry"] = [Point(xy) for xy in zip(anom_df.longitude, anom_df.latitude)]
     anom_gdf = gpd.GeoDataFrame(anom_df, geometry="geometry")
     anom_gdf.set_crs("EPSG:4326", inplace=True)
 
@@ -165,33 +169,44 @@ for yr in set(gdf['year']):
     anom_by_year[yr] = anom_gdf
 
 
+###
+all_dfs = []
+for i in gdf_agg.index:
+    if (i % 1 == 0):
+        print("... ", i, " / ", len(gdf_agg.index))
+
+    yy = []
+    for yr in range(min_year, max_year+1):
+        var_gdf         = anom_by_year[yr]
+        joined_gdf      = gpd.sjoin(var_gdf, gdf_agg.iloc[[i]], how='left', predicate='within')
+        cleaned_gdf     = joined_gdf.dropna(subset=['loc_id'])
+        spat_avg_val    = cleaned_gdf['value'].mean()
+        yy.append(spat_avg_val)
+
+    years = np.arange(min_year, max_year+1)
+    yy_dt = detrend(yy)
+    yy_dt = np.array(yy_dt)
+    mu = yy_dt.mean()
+    sigma = yy_dt.std(ddof=1)   # population stdev; use ddof=1 for sample stdev
+    yy_std = (yy_dt - mu) / sigma
+
+    df_anom = pd.DataFrame({
+    'tp_anom': yy_std,
+    'year':    years,
+    'loc_id':  gdf_agg.iloc[i]['loc_id']      # this string is broadcast to every row
+    })
+
+    all_dfs.append(df_anom)
 
 
-anom_vals = []
-for i in gdf.index:
-    if (i % 100 == 0):
-        print("... ", i, " / ", len(gdf.index))
-    yr = gdf['year'].iloc[i]
-    # anom = var1_yr.sel(year=yr)
+df_all = pd.concat(all_dfs, ignore_index=True)
+print(df_all)
 
-    # varname = list(anom.data_vars)[0]
-    # anom = anom[varname]
+merged = gdf.merge(
+    df_all,
+    on=['loc_id','year'],
+    how='left'        # or 'left', 'right', 'outer' depending on what you need
+)
 
-    # anom_df = anom.to_dataframe(name="value").reset_index()
-    # anom_df["geometry"] = [Point(xy) for xy in zip(anom_df.lon, anom_df.lat)]
-    # anom_gdf = gpd.GeoDataFrame(anom_df, geometry="geometry")
-    # anom_gdf.set_crs("EPSG:4326", inplace=True)
-
-    # anom_gdf = anom_gdf[["year", "value", "geometry"]]
-    anom_gdf = anom_by_year[yr]
-    joined_gdf = gpd.sjoin(anom_gdf, gdf.iloc[[i]], how='left', predicate='within')
-    cleaned_gdf = joined_gdf.dropna(subset=['loc_id'])
-
-    mean_anom = cleaned_gdf['value'].mean()
-    # print("...... ", np.round(mean_anom,3))
-    anom_vals.append(mean_anom)
-
-gdf['anom_tp'] = anom_vals
-
-df = gdf.drop(columns="geometry")
-df.to_csv("/Users/tylerbagwell/Desktop/differential_MEANanom_tp.csv", index=False)
+merged = merged.drop(columns="geometry")
+merged.to_csv("/Users/tylerbagwell/Desktop/YearlyAnom_tp_DMItype2_Global_square4_19502023.csv", index=False)
