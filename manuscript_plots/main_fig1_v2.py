3fe18e54a89e1b4dbe6ffd4364ba8f2d42a4ca56
tyler-wentai <teb6@rice.edu>
2025-07-08 07:58:48 -0700

    .

--- manuscript_plots/main_fig1_v2.py
+++ manuscript_plots/main_fig1_v2.py
@@ -13,6 +13,9 @@ from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 import matplotlib.colors as mcolors
 import matplotlib.patches as mpatches
 import cartopy.feature as cfeature
+from matplotlib.patches import Patch
+import matplotlib.ticker as mticker
+from calc_annual_index import *
 
 print('\n\nSTART ---------------------\n')
 
@@ -54,9 +57,77 @@ gdf_onset = gpd.GeoDataFrame(
 )
 
 
+start_year, end_year = 1950, 2023
+
+nino3_monthly = prepare_NINO3(file_path='data/NOAA_NINO3_data.txt',
+                                start_date=datetime(start_year, 1, 1, 0, 0, 0),
+                                end_date=datetime(end_year, 12, 1, 0, 0, 0))
+dmi_monthly = prepare_DMI(file_path = 'data/NOAA_DMI_data.txt',
+                                start_date=datetime(start_year, 1, 1, 0, 0, 0),
+                                end_date=datetime(end_year, 12, 1, 0, 0, 0))
+
+nino3_yearly    = compute_annualized_index('nino3', start_year, end_year+1)
+dmi_yearly      = compute_annualized_index('dmi', start_year, end_year)
+
+nino3_std   = nino3_yearly['cindex'].std()
+dmi_std     = dmi_yearly['cindex'].std()
+
+sigma_nino3 = nino3_monthly['ANOM'].std()
+sigma_dmi = dmi_monthly['ANOM'].std()
+
+nino3_yearly['year'] = pd.to_datetime(nino3_yearly['year'].astype(str), format='%Y')
+dmi_yearly['year'] = pd.to_datetime(dmi_yearly['year'].astype(str), format='%Y')
+
+import seaborn as sns
+cmap = sns.diverging_palette(220, 20, as_cmap=True)
+num_colors = 3
+levels = np.linspace(0, 1, num_colors)
+colors = [cmap(level) for level in levels]
+
+# assign colors
+def pick_color_nino3(x):
+    if x < -sigma_nino3:
+        return 'blue'
+    elif x > sigma_nino3:
+        return 'red'
+    else:
+        return 'gray'
+def pick_color_dmi(x):
+    if x < -sigma_dmi:
+        return colors[0]
+    elif x > sigma_dmi:
+        return colors[2]
+    else:
+        return 'gray'
+colors_nino3 = nino3_yearly['cindex'].map(pick_color_nino3)
+colors_dmi = dmi_yearly['cindex'].map(pick_color_dmi)
+
+
+# build legend patches
+legend_patches_nino3 = [
+    Patch(color='red',   alpha=0.6, label='El Niño'),
+    Patch(color='gray',  alpha=0.6, label='Neutral'),
+    Patch(color='blue',  alpha=0.6, label='La Niña'),
+]
+legend_patches_dmi = [
+    Patch(color=colors[2],   alpha=0.6, label='+IOD'),
+    Patch(color='gray',  alpha=0.6, label='Neutral'),
+    Patch(color=colors[0],  alpha=0.6, label='-IOD'),
+]
+
+
 # Define a polygon with lat/lon coordinates
-fig, ax = plt.subplots(figsize=(8, 4), subplot_kw={'projection': ccrs.Robinson()})
-gl = ax.gridlines(
+fig = plt.figure(figsize=(8.0, 10))
+
+# ── lay out a 3 × 1 grid ───────────────────────────────
+gs   = fig.add_gridspec(nrows=3, ncols=1, hspace=0.3, height_ratios=[3, 1, 1])
+
+ax1  = fig.add_subplot(gs[0, 0], projection=ccrs.Robinson())  # map axis
+ax2  = fig.add_subplot(gs[1, 0])                              # normal axis
+ax3  = fig.add_subplot(gs[2, 0])                              # normal axis
+
+# fig, ax1 = plt.subplots(figsize=(8, 4), subplot_kw={'projection': ccrs.Robinson()})
+gl = ax1.gridlines(
 crs=ccrs.PlateCarree(),
 draw_labels={
     'bottom': True,
@@ -65,7 +136,7 @@ draw_labels={
     'right':  False
 },
 linewidth=0.4)
-gl.xlocator = mticker.FixedLocator(range(-180, 181, 60))  # meridians every 60°
+gl.xlocator = mticker.FixedLocator(range(-180, 181, 30))  # meridians every 60°
 gl.ylocator = mticker.FixedLocator(range(-60, 61, 30))    # parallels every 30°
 gl.xlabel_style = {'size': 10}
 gl.ylabel_style = {'size': 10}
@@ -75,11 +146,11 @@ gl.top_labels   = False
 gl.right_labels = False
 
 
-ax.set_global()
-ax.coastlines()
+ax1.set_global()
+ax1.coastlines()
 
-ax.add_feature(cfeature.LAND, facecolor='gainsboro', alpha=0.5)
-# ax.add_feature(cfeature.OCEAN, facecolor='lightblue')
+ax1.add_feature(cfeature.LAND, facecolor='gainsboro', alpha=0.5)
+# ax1.add_feature(cfeature.OCEAN, facecolor='lightblue')
 
 index_box = mpatches.Rectangle((-150, -5), 60, 10, 
                                fill=True, facecolor='purple', edgecolor='purple', linewidth=1.5, alpha=0.20,
@@ -90,9 +161,9 @@ index_box1 = mpatches.Rectangle((50, -10), 20, 20,
 index_box2 = mpatches.Rectangle((90, -10), 20, 10, 
                                 fill=True, facecolor='purple', edgecolor='purple', linewidth=1.5, alpha=0.20,
                                 transform=ccrs.PlateCarree())
-ax.add_patch(index_box)
-ax.add_patch(index_box1)
-ax.add_patch(index_box2)
+ax1.add_patch(index_box)
+ax1.add_patch(index_box1)
+ax1.add_patch(index_box2)
 
 
 
@@ -108,11 +179,13 @@ markers = ['o', 's', '^', 'v', '<', '>', 'd', 'p', 'h']
 
 x, y = gdf_onset['onset_lon'].values, gdf_onset['onset_lat'].values
 max_year = gdf_onset['year'].max()
+counts = []
 for i, dec in enumerate(decades):
     subset = gdf_onset[gdf_onset['decade_start'] == dec]
+    counts.append(subset.shape[0])
     end = min(dec + 9, max_year)              # cap at 2023
     label = f"{dec}-{end}"
-    ax.scatter(
+    ax1.scatter(
         subset['onset_lon'],
         subset['onset_lat'],
         marker=markers[i % len(markers)],
@@ -126,31 +199,144 @@ for i, dec in enumerate(decades):
         linewidth=0.5
     )
 
+ax1.set_title('Spatio-Temporal Distribution of Conflict Onsets, 1950-2023', fontsize=12)
 
-ax.text(0.095, 0.485, 'NINO3 region', transform=ax.transAxes, fontsize=10)
-ax.text(0.75, 0.35, 'DMI regions',  transform=ax.transAxes, fontsize=10)
+# bar plot
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+axmap = ax1                          # <- the Cartopy axis you already have
+axbar = inset_axes(
+    axmap,
+    width="14%", height="19%",      # 25 % of the map-axis width, full height
+    loc="lower left",               # attach on the left of the inset-box …
+    bbox_to_anchor=(0.15, 0.20, 1, 1),  # … which we shift just outside (x=1.02)
+    bbox_transform=axmap.transAxes,
+    borderpad=0,
+)
+
+# decades = ['50', '60', '70', '80', '90', '00', '10', '20']
+x_pos = np.arange(len(counts))
+axbar.bar(x_pos, counts, color="gray", alpha=0.6)
+axbar.set_xticks(x_pos)
+axbar.set_xticklabels([f"{d}s" for d in decades], rotation=90, fontsize=7)
+axbar.tick_params(axis="y", labelrotation=90, labelsize=7)
+axbar.set_ylabel("No. of\nonsets", fontsize=8)
+
+axbar.tick_params(axis="both",          # "x", "y", or "both"
+               which="both",         # major & minor
+               direction="in")
+# axbar.spines["left"].set_visible(False)   # aesthetic: hide shared border
+
+
+#
+ax1.text(0.090, 0.485, 'NINO3 region', transform=ax1.transAxes, fontsize=9)
+ax1.text(0.655, 0.34, 'DMI regions',  transform=ax1.transAxes, fontsize=9)
+
+# ax.plot([0.66, 0.72], [0.45, 0.39], color='k', linewidth=1.5, transform=ax.transAxes)
+from matplotlib.patches import FancyArrowPatch
+arrow1 = FancyArrowPatch(
+    [0.72, 0.39], [0.66, 0.45], 
+    arrowstyle="-|>", mutation_scale=10,
+    color="k", lw=1,
+    transform=ax1.transAxes   # omit if not on a map
+)
+arrow2 = FancyArrowPatch(
+    [0.72, 0.39], [0.79, 0.45], 
+    arrowstyle="-|>", mutation_scale=10,
+    color="k", lw=1,
+    transform=ax1.transAxes   # omit if not on a map
+)
+ax1.add_patch(arrow1)
+ax1.add_patch(arrow2)
 
 
 plt.subplots_adjust(right=0.8)  
-ax.legend(
+ax1.legend(
     title="Onset decade",
-    fontsize=9,
-    loc="center left",  
-    bbox_to_anchor=(1.02, 0.5),  # x=1.02 just to the right of the axes, y=0.5 = middle
-    borderaxespad=0.0,
+    title_fontsize=10,
+    fontsize=10,
+    loc="upper center",           # anchor the *top-centre* of the legend…
+    bbox_to_anchor=(0.5, -0.08),  # …to a point 8 % of the axis height below it
+    borderaxespad=0,
     framealpha=0.7,
-    markerscale=1.5,
-    frameon=False
+    markerscale=1.75,
+    frameon=False,
+    ncol=4                        # spread labels in a row if you like
 )
 
-ax.text(0.05, 0.98, 'a', transform=ax.transAxes, fontsize=14, bbox=dict(
-        boxstyle='square,pad=0.2',  # try 'square', 'round', 'larrow', etc.
-        facecolor='white',         # box fill color
-        edgecolor='black',         # box edge color
-        linewidth=0.5                # edge line width
-    ))
+ax1.text(+0.00, 1.0, 'a', transform=ax1.transAxes, ha="center", va="center",
+         fontsize=14, bbox=dict(boxstyle='square,pad=0.2',  # try 'square', 'round', 'larrow', etc.
+            facecolor='white',          # box fill color
+            edgecolor='black',          # box edge color
+            linewidth=0.5))             # edge line width
 
-plt.title('Spatio-temporal distribution of conflict onsets, n=555', fontsize=12)
+
+# — Panel B —
+ax2.plot(
+    nino3_yearly['year'],
+    nino3_yearly['cindex'],
+    color='black',
+    linewidth=1.5,
+    zorder=1
+)
+ax2.bar(
+    nino3_yearly['year'],
+    nino3_yearly['cindex'],
+    width=300,
+    alpha=0.7,
+    color=colors_nino3,
+    zorder=0
+)
+ax2.axhline(0, color='black', linewidth=1.0, linestyle='-')
+ax2.set_ylabel('NDJ-Avg. NINO3 (°C)')
+ax2.set_title('Annualized Index for the El Niño-Southern Oscillation')
+ax2.grid(True, linestyle='--', alpha=0.5)
+ax2.text(+0.00, 1.15, 'b', transform=ax2.transAxes, ha="center", va="center",
+         fontsize=14, bbox=dict(boxstyle='square,pad=0.2',  # try 'square', 'round', 'larrow', etc.
+            facecolor='white',          # box fill color
+            edgecolor='black',          # box edge color
+            linewidth=0.5))             # edge line width
+ax2.legend(
+    handles=legend_patches_nino3,
+    loc='upper left',
+    frameon=False,
+    fontsize=9
+)
+ax2.yaxis.set_major_formatter(mticker.FormatStrFormatter('%.1f'))
+
+# — Panel C —
+ax3.plot(
+    dmi_yearly['year'],
+    dmi_yearly['cindex'],
+    color='black',
+    linewidth=1.5,
+    zorder=1
+)
+ax3.bar(
+    dmi_yearly['year'],
+    dmi_yearly['cindex'],
+    width=300,
+    alpha=0.7,
+    color=colors_dmi,
+    zorder=0
+)
+ax3.axhline(0, color='black', linewidth=1.0, linestyle='-')
+ax3.set_ylabel('SON-Avg. DMI (°C)')
+ax3.set_title('Annualized Index for the Indian Ocean Dipole')
+ax3.grid(True, linestyle='--', alpha=0.5)
+ax3.text(+0.00, 1.15, 'c', transform=ax3.transAxes, ha="center", va="center",
+         fontsize=14, bbox=dict(boxstyle='square,pad=0.2',  # try 'square', 'round', 'larrow', etc.
+            facecolor='white',          # box fill color
+            edgecolor='black',          # box edge color
+            linewidth=0.5))             # edge line width
+ax3.set_xlabel('Year')
+ax3.legend(
+    handles=legend_patches_dmi,
+    loc='upper left',
+    frameon=False,
+    fontsize=9
+)
+ax3.yaxis.set_major_formatter(mticker.FormatStrFormatter('%.1f'))
+
 plt.tight_layout()
-# plt.savefig('/Users/tylerbagwell/Desktop/RobMAP_NINO3type2_psi_raw.png', dpi=300, bbox_inches='tight', pad_inches=0.1)
+plt.savefig('/Users/tylerbagwell/Desktop/manuscript_plots/Main_fig1_v2.png', dpi=300, bbox_inches='tight', pad_inches=0.1)
 plt.show()
