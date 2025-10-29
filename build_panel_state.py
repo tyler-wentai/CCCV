520b2079a1072f2436164ad9dad5ad5e2748ec8b
tyler-wentai <teb6@rice.edu>
2025-10-29 13:50:19 -0500

    .

--- build_panel_state.py
+++ build_panel_state.py
@@ -306,13 +306,13 @@ def initalize_state_onset_panel(panel_start_year, panel_end_year, telecon_path, 
 
 panel = initalize_state_onset_panel(panel_start_year=1950,
                                     panel_end_year=2023,
-                                    telecon_path = '/Users/tylerbagwell/Documents/Rice_University/CCCV/data/cccv_data/processed_teleconnections/psi_NINO3_type2_GPCC.nc',
+                                    telecon_path = '/Users/tylerbagwell/Documents/Rice_University/CCCV/data/cccv_data/processed_teleconnections/psi_pdsiNINO3.nc',
                                     pop_path = '/Users/tylerbagwell/Documents/Rice_University/CCCV/data/cccv_data/gpw-v4-population-count-rev11_totpop_15_min_nc/gpw_v4_population_count_rev11_15_min.nc',
                                     clim_index='nino3',
                                     response_var = 'binary',
                                     plot_telecon=True)
 # panel.to_csv('/Users/tylerbagwell/Desktop/panel_datasets/onset_datasets_state/Onset_Binary_GlobalState_NINO3type2.csv', index=False)
-panel.to_csv('/Users/tylerbagwell/Documents/Rice_University/CCCV/data/panel_datasets/onset_datasets_state/Onset_Binary_GlobalState_NINO3type2_GPCC.csv', index=False)
+panel.to_csv('/Users/tylerbagwell/Documents/Rice_University/CCCV/data/panel_datasets/onset_datasets_state/Onset_Binary_GlobalState_pdsiNINO3.csv', index=False)
 # panel.to_csv('/Users/tylerbagwell/Desktop/panel_datasets/onset_datasets_state/Onset_Binary_GlobalState_mrsosNINO3_wGeometry.csv', index=False)
 print(panel)
 
